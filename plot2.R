bae4aca223e9957b9282c7abeb030c139a19c9f8
Thiago F Pappacena <pappacena@gmail.com>
2014-05-23 18:44:25 -0300

    finishing question 2

---
+++ plot2.R
@@ -0,0 +1,38 @@
+if(!"data" %in% dir()) {
+    stop("data directory not found. Did you set your working directory?")
+}
+
+if(!file.exists("./data/NEI_data.zip")) {
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",
+                  "data/NEI_data.zip")
+}
+
+if(!file.exists("./data/summarySCC_PM25.rds") ||
+       !file.exists("./data/Source_Classification_Code.rds")) {
+    unzip("./data/NEI_data.zip", exdir="data")
+}
+
+
+# Loads only if not in environment
+if(!exists("NEI")) {
+    NEI <- readRDS("data/summarySCC_PM25.rds")
+}
+if(!exists("SCC")) {
+    SCC <- readRDS("data/Source_Classification_Code.rds")
+}
+
+if(!exists("full_dataset")) {
+    full_dataset <- merge(NEI, SCC, by="SCC")
+}
+
+
+to_plot <- subset(full_dataset, fips == "24510")
+to_plot <- to_plot[c("year", "Emissions")]
+agg <- aggregate(to_plot$Emissions, list(Year=to_plot$year), sum)
+names(agg) <- c("Year", "Sum.Emissions")
+
+with(agg, {
+    png(file = "plot2.png", bg = "transparent")
+    plot(Year, Sum.Emissions, type="line")
+    dev.off()
+})
